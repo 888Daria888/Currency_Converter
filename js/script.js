78f3694c5705546bf534f3b8a083fba3bd1fc9f7
888Daria888 <malahovadasa@gmail.com>
2022-05-04 23:17:15 +0300

    js_80

--- js/script.js
+++ js/script.js
@@ -1,72 +1,78 @@
-// let avaliableInput = document.querySelector()
-// let desirableInput = document.querySelectorAll()
-// let currencyAv = document.querySelectorAll(ВСЕ ЛИШКИ, НА ВСЕХ НАВЕСИТЬ СЛУШАТЕЛЯ)
-// let currencyDes
-
-// // isAveliableInput
 
 
+let isAveliableInput = document.querySelector('.input-text')
 
-// currencyAv.forEach(item => {
-//    item.addEventListener('click', () =>{
-//       //снимать со всех эл класс актив
-//       currencyAv.forEach(item => {
-//          item.classList.remove('chosen')
-//       })
-//    }) event.target.classList.add('chosen');
-//    getCurrencyCourse(true)//флаг
-// })
-// // нужно будет сделать флаг каким импутом мы ппользуемся
-
-
-// function getCurrencyCourse(isAveliableInput = true){
-//    let rightCurrency = querySelector().innetText //USD
-//    let leftCurrency = //RUB
-//    let avaliableCourse
-//    let discrablCourse
-//     //запрос не делаем, а просто подставляем значение )Х
-//    if(rightCurrency === leftCurrency){
-//       avaliableCourse.innetText = `1 ${rightCurrency} = 1.00 ${leftCurrency}`
-//       desirableInput.value = avaliableInput.value
-//    } else {
-//       fetch()
-//    let rightCurrency = querySelector().innetText //USD
-//    let leftCurrency = //RUB
-//    var requestURL = `https://api.exchangerate.host/from ${rightCurrency} ${leftCurrency} `;
-//    .then
-//    let rateAmount = data.rates[leftCurrency]
-//    }
-// }
 
 let menu = document.querySelectorAll('.header-list')
 for (let i = 0; i < menu.length; i++) {
    menu[i].addEventListener("click", function () {
       console.log(menu[i]);
-      
       let statusActivMenu = document.getElementsByClassName("menu_activ");
       console.log(statusActivMenu);
-      
       statusActivMenu[0].className = statusActivMenu[0].className.replace("menu_activ", "");
       menu[i].className += " menu_activ";
    });
 }
 
-let haveList = document.querySelectorAll('.have-list')
-console.log(haveList);
-console.log(haveList[0]);
 
-for (let i = 0; i < haveList.length; i++) {
-   haveList[i].addEventListener("click", function () {
-      console.log(haveList[i]);
-      let haveListActiv = document.getElementsByClassName("have-list_activ");
-      console.log(haveListActiv);
+
+let haveList = document.querySelectorAll('.have__content .content_list li');
+haveList.forEach(item => {
+   if (haveList[0] || haveList[3]) {
+      haveList[0].style.borderRadius = '3px 0px 0px 3px';
+      haveList[3].style.borderRadius = '0px 3px 3px 0px';
+   }
+   item.addEventListener('click', (event) => {
+      haveList.forEach(item => {
+         item.classList.remove('active');
+      });
+      event.target.classList.add('active');
+      getCurrencyCourse()
+   });
+});
+
+let wantList = document.querySelectorAll('.want__content .content_list li');
+wantList.forEach(item => {
+   if (wantList[0] || wantList[3]) {
+      wantList[0].style.borderRadius = '3px 0px 0px 3px';
+      wantList[3].style.borderRadius = '0px 3px 3px 0px';
+   }
+   item.addEventListener('click', (event) => {
+      wantList.forEach(item => {
+         item.classList.remove('active');
+      });
+      event.target.classList.add('active');
+      getCurrencyCourse()
+   });
+});
+
+
+
+function getCurrencyCourse() {
+   let haveCurrency = document.querySelector('.have-currency')
+   let wantCurrency = document.querySelector('.want-currency')
+   let activeCurrency = document.querySelectorAll('.active')
+   let x = activeCurrency[0].innerText
+   let y = activeCurrency[1].innerText
+   let haveInputCours = document.querySelector('.have_text')
+   let wantInputCours = document.querySelector('.want_text')
+   if (x === y) {
+      wantInputCours.value = haveInputCours.value
       
-      haveListActiv[0].className= haveListActiv[0].className.replace(" have-list_activ", "");
-      haveList[i].className += " have-list_activ";
-// как лучше бордер радиус присвоить после активного тега 
-   })
+      haveCurrency.innerHTML = `1 ${x} = 1.00 ${y}`
+      wantCurrency.innerHTML = `1 ${x} = 1.00 ${y}`
+   }
+   else {
+      fetch(
+         `https://api.exchangerate.host/latest?base=${x}&symbols=${y}`
+      )
+         .then((res) => res.json())
+         .then((data) => {
+            let summCourseY = haveInputCours.value * data.rates[y]
+            wantInputCours.value = summCourseY
+            haveCurrency.innerHTML = `${haveInputCours.value} ${x} = ${wantInputCours.value} ${y}`
+            wantCurrency.innerHTML = `${wantInputCours.value} ${y} = ${haveInputCours.value} ${x}`
+         })
+         .catch((err)=>alert('упс.что-то пошло не так'))
+   }
 }
-
-
-
-
