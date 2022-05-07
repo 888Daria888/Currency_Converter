1b15804a17be3cca7ea339cbebe548f2bcf38308
888Daria888 <malahovadasa@gmail.com>
2022-05-07 12:57:10 +0300

    test_2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-
-
-let isAveliableInput = document.querySelector('.input-text')
-
+let statusActivMenu = document.getElementsByClassName("menu_activ");
+let haveInputCours = document.querySelector('.have_text')
+let wantInputCours = document.querySelector('.want_text')
+let haveInfo = document.querySelector('.have-info')
+let wantInfo = document.querySelector('.want-info')
+let haveValue = 'RUB'
+let wantValue = 'USD'
 
 let menu = document.querySelectorAll('.header-list')
 for (let i = 0; i < menu.length; i++) {
    menu[i].addEventListener("click", function () {
       console.log(menu[i]);
-      let statusActivMenu = document.getElementsByClassName("menu_activ");
       console.log(statusActivMenu);
       statusActivMenu[0].className = statusActivMenu[0].className.replace("menu_activ", "");
       menu[i].className += " menu_activ";
@@ -16,7 +18,7 @@ for (let i = 0; i < menu.length; i++) {
 
 
 
-let haveList = document.querySelectorAll('.have__content .content_list li');
+let haveList = document.querySelectorAll('.have-list');
 haveList.forEach(item => {
    if (haveList[0] || haveList[3]) {
       haveList[0].style.borderRadius = '3px 0px 0px 3px';
@@ -28,10 +30,11 @@ haveList.forEach(item => {
       });
       event.target.classList.add('active');
       getCurrencyCourse()
+      return haveValue = item.value
    });
 });
 
-let wantList = document.querySelectorAll('.want__content .content_list li');
+let wantList = document.querySelectorAll('.want-list');
 wantList.forEach(item => {
    if (wantList[0] || wantList[3]) {
       wantList[0].style.borderRadius = '3px 0px 0px 3px';
@@ -41,41 +44,35 @@ wantList.forEach(item => {
       wantList.forEach(item => {
          item.classList.remove('active');
       });
+      
       event.target.classList.add('active');
       getCurrencyCourse()
+      return wantValue = item.value
    });
 });
 
 
 
 function getCurrencyCourse() {
-   let haveCurrency = document.querySelector('.have-currency')
-   let wantCurrency = document.querySelector('.want-currency')
-   let activeCurrency = document.querySelectorAll('.active')
-   let x = activeCurrency[0].innerText
-   let y = activeCurrency[1].innerText
-   let haveInputCours = document.querySelector('.have_text')
-   let wantInputCours = document.querySelector('.want_text')
-
-   if (x === y) {
+   if (haveValue === wantValue) {
       wantInputCours.value = haveInputCours.value
-      haveCurrency.innerHTML = `1 ${x} = 1.00 ${y}`
-      wantCurrency.innerHTML = `1 ${x} = 1.00 ${y}`
+      haveInfo.innerHTML = `1 ${haveValue} = 1.00 ${wantValue}`
+      wantInfo.innerHTML = `1 ${haveValue} = 1.00 ${wantValue}`
    }
    else {
       fetch(
-         `https://api.exchangerate.host/latest?base=${x}&symbols=${y}&places=4`
+         `https://api.exchangerate.host/latest?base=${haveValue}&symbols=${wantValue}&places=4`
       )
          .then((res) => res.json())
          .then((data) => {
             haveInputCours.addEventListener('input', (e) => {
-               wantInputCours.value = e.target.value * data.rates[y]
-            })
-            wantInputCours.addEventListener('input', (e) => {
-               haveInputCours.value = e.target.value * data.rates[x]
-            })
-            haveCurrency.innerHTML = `${haveInputCours.value} ${x} = ${wantInputCours.value} ${y}`
-            wantCurrency.innerHTML = `${wantInputCours.value} ${y} = ${haveInputCours.value} ${x}`
+            wantInputCours.value = e.target.value * data.rates[wantValue]
+         })
+         wantInputCours.addEventListener('input', (e) => {
+            haveInputCours.value = e.target.value * data.rates[haveValue]
+         })
+            haveCurrency.innerHTML = `${haveInputCours.value} ${haveValue} = ${wantInputCours.value} ${wantValue}`
+            wantCurrency.innerHTML = `${wantInputCours.value} ${wantValue} = ${haveInputCours.value} ${haveValue}`
          })
          .catch((err) => alert('упс.что-то пошло не так'))
    }
